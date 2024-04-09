bcd45697863763d6f534d37db069da4ee91a94cf
Arthur LE-DEVEDEC <ledeveda@iut-acy.local>
2024-04-09 09:10:12 +0200

    0.3

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -44,6 +44,9 @@ class Character
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $creation = null;
 
+    #[ORM\Column(length: 40)]
+    private ?string $identifier = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -170,6 +173,18 @@ class Character
     public function setCreation(\DateTimeInterface $creation): static
     {
         $this->creation = $creation;
+
+        return $this;
+    }
+
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): static
+    {
+        $this->identifier = $identifier;
 
         return $this;
     }
